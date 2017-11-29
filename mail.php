668d9771e4eb34aa2e9973adc1cdbb184ad66463
Victoria Ranieri <victoriaranieri@Victorias-MacBook-Pro.local>
2017-11-28 22:02:25 -0600

    php update

--- mail.php
+++ mail.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
+<?php 
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$admin_email = "youmademe17@gmail.com";
+$to= "youmademe17@gmail.com";
 $subject = "One dot";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader, $phone) or die("Error!");
-echo "Thank You!";
+if ($POST['submit']){
+	if (mail($recipient, $subject, $formcontent, $mailheader, $phone)) {echo 'Thank you!'} 
+		else { echo 'Somethign went wrong, please try again.';
+		}
+}
 ?>
