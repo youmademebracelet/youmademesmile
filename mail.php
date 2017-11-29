829fa62510ad96f8c78bef4eb8661f13a85be0de
Victoria Ranieri <victoriaranieri@Victorias-MacBook-Pro.local>
2017-11-28 22:22:26 -0600

    trial

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST["submit"])) {
-$recipient= "youmademe17@gmail.com";
+$to= "youmademe17@gmail.com";
 $name = $POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,9 +8,8 @@ $formcontent="From: $name \n Message: $message";
 $subject = "One dot";
 $mailheader = "From: $email \r\n";
 }
-if ($POST['submit']){
-	if (mail($subject, $formcontent, $mailheader) {echo 'Thank you!'} 
-		else { echo 'Somethign went wrong, please try again.';
+if (mail($subject, $formcontent, $mailheader) {echo "<p>Thank you!</P>"} 
+		else { echo("<p>Something went wrong, please try again.</p>");
 		}
 }
 ?>
