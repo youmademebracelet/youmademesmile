6d4610428a7ea3655e2e4e987344822a5936900a
Victoria Ranieri <victoriaranieri@Victorias-MacBook-Pro.local>
2017-11-28 22:16:48 -0600

    retry2

--- mail.php
+++ mail.php
@@ -1,13 +1,15 @@
 <?php 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$message = $_REQUEST['message'];
+if(isset($_POST["submit"])) {
+$recipient= "youmademe17@gmail.com";
+$name = $POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$admin_email= "youmademe17@gmail.com";
 $subject = "One dot";
 $mailheader = "From: $email \r\n";
-if ($REQUEST['submit']){
-	if (mail(($subject, $formcontent, $mailheader)) {echo 'Thank you!'} 
+}
+if ($POST['submit']){
+	if (mail($subject, $formcontent, $mailheader) {echo 'Thank you!'} 
 		else { echo 'Somethign went wrong, please try again.';
 		}
 }
