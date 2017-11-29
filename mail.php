04619cf8e47546db2c8ad741b016ef193efc885b
Victoria Ranieri <victoriaranieri@Victorias-MacBook-Pro.local>
2017-11-28 21:50:24 -0600

    Revert "Updated php"
    This reverts commit 4a3149c733bf5f234abef07577227e05e683c35a.

--- mail.php
+++ mail.php
@@ -2,7 +2,9 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$admin_email = "youmademe17@gmail.com";
+$recipient = "youmademe17@gmail.com";
 $subject = "One dot";
 $mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader, $phone) or die("Error!");
+echo "Thank You!";
 ?>
